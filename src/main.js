2ecd7e0de698c9a793e4521fafee9279ad26823e
KunKimlong <kimlongkun2412@gmail.com>
2024-08-27 19:53:14 +0700

    new

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import '@/assets/Js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@/assets/Js/jquery-3.7.1.min.js';
 import '@/assets/Js/script.js';
 import '@/assets/Js/data.js';
-import '@/assets/Style/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '@/assets/Style/style.css';
 // import '@popperjs/core';
 import App from './App.vue'
