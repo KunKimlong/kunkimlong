4ba70738929376f8ff748de26a5caff2a27d7655
KunKimlong <kimlongkun2412@gmail.com>
2024-08-27 22:16:22 +0700

    new

--- src/assets/Js/project.js
+++ src/assets/Js/project.js
@@ -1,12 +1,12 @@
 const project = [
     {
         title:'Web application with Laravel project',
-        thumbnail:'/src/assets/Image/kh-fashion.png',
+        thumbnail:'src/assets/Image/kh-fashion.png',
         sourceCode:'https://github.com/KunKimlong/myfinalproject',
     },
     {
         title:'React + Node js',
-        thumbnail:'/src/assets/Image/image-not-found.png',
+        thumbnail:'src/assets/Image/image-not-found.png',
         sourceCode:'https://github.com/KunKimlong/reactandnode',
     },
 ]
